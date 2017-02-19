4d0d2df31ff7f951d88133d71ab2cc3457463361
Geir Skjotskift <geir@mnemonic.no>
2017-02-19 14:58:50 +0100

    removed unused variables

--- pdblist.py
+++ pdblist.py
@@ -35,18 +35,6 @@ import volatility.plugins.common as common
 import volatility.utils as utils
 import volatility.win32 as win32
 
-IMAGE_DEBUG_TYPE_UNKNOWN          = 0
-IMAGE_DEBUG_TYPE_COFF             = 1
-IMAGE_DEBUG_TYPE_CODEVIEW         = 2
-IMAGE_DEBUG_TYPE_FPO              = 3
-IMAGE_DEBUG_TYPE_MISC             = 4
-IMAGE_DEBUG_TYPE_EXCEPTION        = 5
-IMAGE_DEBUG_TYPE_FIXUP            = 6
-IMAGE_DEBUG_TYPE_OMAP_TO_SRC      = 7
-IMAGE_DEBUG_TYPE_OMAP_FROM_SRC    = 8
-IMAGE_DEBUG_TYPE_BORLAND          = 9
-IMAGE_DEBUG_TYPE_RESERVED10       = 10
-IMAGE_DEBUG_TYPE_CLSID            = 11
 
 datatypes = {
         '_PDBGUID'      : [ 0x10, {
